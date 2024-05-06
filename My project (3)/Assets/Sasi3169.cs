896142e8da1c785490e364d8550e96e405c71e67
Eurnol <dogukanozcan@hotmail.com.tr>
2024-05-06 19:14:01 +0300

    Sa

---
+++ My project (3)/Assets/Sasi3169.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sasi3169 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SA
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
